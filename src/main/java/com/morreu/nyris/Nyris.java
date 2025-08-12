4ced43f2fd6f0acc8f57aa080aca6ca5dcaeabc5
Vinicius <vanusaspassos77@gmail.com>
2025-08-12 14:34:57 -0300

    vGalinha - Estrutura do inicio do plugin

---
+++ src/main/java/com/morreu/nyris/Nyris.java
@@ -0,0 +1,17 @@
+package com.morreu.nyris;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Nyris extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
